55f98817e0251853993ad3ea432abe4fe484ab71
dineshvkumarv <dineshkumar7259764116@gmail.com>
2024-10-08 12:45:26 +0530

    delete files

---
+++ delete.sh
@@ -0,0 +1,13 @@
+git add maven_java_web_example-master/hello.txt  maven_java_web_example-master/mvnw    maven_java_web_example-master/mvnw.cmd
+
+git commit -m "delete files " maven_java_web_example-master/hello.txt  maven_java_web_example-master/mvnw    maven_java_web_example-master/mvnw.cmd
+
+git push
+
+git rm delete.sh
+ 
+git add delete.sh
+
+git commit -m "delete files" delete.sh
+
+git push
