9b6f3f7c77a7b180d286c2a72ee245f7b7818eab
dineshvkumarv <dineshkumar7259764116@gmail.com>
2024-10-11 20:02:35 +0530

    login info

--- login.java
+++ login.java
@@ -1,12 +1,12 @@
-public class LoginUser
 {
 	public static void main(String args[]){
 
 	int i;
 	
-	for(i=1;i<=80;i++){
-		System.out.println("Number:"+i);
+	for(i=1;i<=30;i++){
+		System.out.println("Number: " + i);
 	}
 
 }
 }
+
